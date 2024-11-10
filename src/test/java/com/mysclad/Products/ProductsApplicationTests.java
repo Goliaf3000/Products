fd92382292ba182f07fe2b86ce8504d3c6a89cf5
Суслин Андрей <andriw_suslin@mail.ru>
2024-11-10 11:50:22 +0300

    Initial commit

---
+++ src/test/java/com/mysclad/Products/ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysclad.Products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
